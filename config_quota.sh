8793bf053531850b3abb8159446899a6030c73e8
Lara Pruna Ternero <laraprn.12@gmail.com>
2021-04-14 19:20:04 +0200

    últimos retoques al programa config_quota y a las funciones de la librería

--- config_quota.sh
+++ config_quota.sh
@@ -40,39 +40,142 @@ if [[ $(f_eres_root;echo $?) = 0 ]]; then
 else
 	echo 'Tienes que ser root.'
 	exit
+fi
+
 echo 'Lista de volúmenes:'
 lsblk -l
 
-echo 'Introduce el nombre de un dispositivo de bloque sin montar:'
+echo 'Introduce el nombre de un dispositivo de la lista que no esté montado:'
 read volumen
-if [[ $(f_está_montado $volumen) = 1 ]]; then
-	if [[ $(f_existe_directorio /QUOTA; echo $?) = 1 ]]; then
-		echo 'Creando directorio /QUOTA...'
-		mkdir /QUOTA
-	fi
-	echo 'Montando dispositivo en el directorio /QUOTA.'
-	f_montar $volumen /QUOTA
-	echo 'Dispositivo montado.'
-
-else
-	if [[ $(df | egrep $volumen | awk '{print $6}') = '/QUOTA' ]]; then
-		echo 'El dispositivo está montado en /QUOTA.'
-	else
-		echo 'El dispositivo está montado en otro directorio. ¿Desea desmontarlo? (s/n)'
-		read resp4
-		if [[ $resp4 = 's' ]]; then
-			echo 'Desmontando dispositivo...'
-			umount $volumen
+if [[ $(lsblk -l | egrep $volumen | awk '{print $6}') = 'part' ]]; then
+	if [[ $(lsblk -l | egrep $volumen) ]]; then
+		if [[ $(f_esta_montado /dev/$volumen;echo $?) = 1 ]]; then
 			if [[ $(f_existe_directorio /QUOTA; echo $?) = 1 ]]; then
 				echo 'Creando directorio /QUOTA...'
 				mkdir /QUOTA
 			fi
+			echo '¿Qué tipo de sistema de ficheros desea crear en el volumen? (ext2/ext3/ext4/vfat)'
+			read fs
+			echo 'Creando sistema de ficheros en el volumen.'
+			f_crea_fs /dev/$volumen $fs
 			echo 'Montando dispositivo en el directorio /QUOTA.'
-			f_montar $volumen /QUOTA
+			f_montar /dev/$volumen /QUOTA
 			echo 'Dispositivo montado.'
 		else
-			echo 'Fin del programa.'
-			exit
+			if [[ $(df | egrep /dev/$volumen | awk '{print $6}') = '/QUOTA' ]]; then
+				echo 'El dispositivo está montado en /QUOTA.'
+				fs=$(lsblk -f | egrep $volumen | awk '{print $2}')
+			else
+				echo 'El dispositivo está montado en otro directorio. ¿Desea desmontarlo? (s/n)'
+				read resp4
+				if [[ $resp4 = 's' ]]; then
+					echo 'Desmontando dispositivo...'
+					umount /dev/$volumen
+					if [[ $(f_existe_directorio /QUOTA; echo $?) = 1 ]]; then
+						echo 'Creando directorio /QUOTA...'
+						mkdir /QUOTA
+					fi
+					echo 'Montando dispositivo en el directorio /QUOTA.'
+					f_montar /dev/$volumen /QUOTA
+					echo 'Dispositivo montado.'
+				else
+					echo 'Fin del programa.'
+					exit
+				fi
+			fi
+		fi
+	else
+		echo 'Nombre de volumen incorrecto.'
+		echo 'Fin del programa.'
+		exit
+	fi
+else
+	echo 'Ese dispositivo no tiene UUID. ¿Desea crear una nueva partición? (s/n)' 
+	read part
+	if [[ $part = 's' ]]; then
+		sgdisk -n 0:0:0 /dev/$volumen > /dev/null
+		echo 'Partición creada. Vuelva a iniciar el programa.'
+		exit
+	else
+		echo 'Fin del programa.'
+		exit
+	fi
+fi
+
+echo 'Comprobando fichero /etc/fstab...'
+UUID=$(f_UUID /dev/$volumen)
+if [[ $(egrep $UUID /etc/fstab) ]]; then
+	echo 'El fichero fstab ya contiene una línea para el volumen.'
+	echo 'Añadiendo opciones de quota...'
+	f_cuotas_fstab $UUID
+	echo 'Opciones de cuotas añadidas a fstab.'
+else
+	echo 'El fichero fstab no contiene información sobre el volumen.'
+	echo 'Introduzca la siguiente información sobre el volumen:'
+	echo 'Opciones de montaje (por defecto, "defaults,usrquota,grpquota"):'
+	read opciones
+	echo 'Número de copias de respaldo (por defecto, 0):'
+	read dump
+	echo 'Orden de chequeo (por defecto, 2):'
+	read fsck
+	if [[ $opciones = $null ]]; then
+		opciones='defaults,usrquota,grpquota'
+	fi
+	if [[ $dump = $null ]]; then
+		dump=0
+	fi
+	if [[ $fsck = $null ]]; then
+		fsck=2
+	fi
+	f_nueva_linea_fstab UUID=$UUID /QUOTA $fs $opciones $dump $fsck
+fi
+echo 'Comprobando ficheros de quota...'
+mount -o remount /QUOTA
+if [[ $(f_existe_fichero aquota.user /QUOTA;echo $?) = 0 ]]; then
+	if [[ $(f_existe_fichero aquota.group /QUOTA;echo $?) = 0 ]]; then
+		echo 'Los ficheros de quota ya están generados.'
+	else
+		echo 'Generando fichero aquota.group...'
+		f_habilita_quota /QUOTA
+		echo 'Ficheros generados.'
+	fi
+else
+	echo 'Generando ficheros de quota...'
+	f_habilita_quota /QUOTA
+	echo 'Ficheros generados.'
+fi
+echo 'Comprobando estado del paquete quotatool...'
+if [[ $(f_esta_instalado quotatool;echo $?) = 0 ]]; then
+	if [[ $(f_paquete_esta_actualizado quotatool; echo $?) = 0 ]]; then
+		echo 'El paquete quotatool está instalado y en su última versión.'
+		echo 'Procediendo a crear una plantilla de cuotas...'
+	else
+		echo 'El paquete quotatool está instalado en una versión anterior. ¿Desea actualizarlo? (s/n)'
+		read resp5
+		if [[ $resp5 = 's' ]]; then
+			echo 'Actualizando quotatool...'
+			f_actualizar_paquete quotatool
+			echo 'Quotatool actualizado.'
+			echo 'Procediendo a crear la plantilla de cuotas...'
+		else
+			echo 'Procediendo a crear la plantilla de cuotas...'
 		fi
 	fi
+else
+	echo 'Instalando paquete quotatool...'
+	f_instalar quotatool
+	echo 'Paquete quotatool instalado.'
+	echo 'Procediendo a crear una plantilla de cuotas...'
+fi
+f_plantilla_quota /QUOTA
+echo 'Plantilla creada.'
+echo '¿Desea aplicar la plantilla en algún usuario o grupo? (s/n)'
+read resp6
+if [[ $resp6 = 's' ]]; then
+	echo '¿A cuántos usuarios quieres copiar la plantilla?'
+	read num
+	f_copia_cuotas $num
+	echo 'Cuotas copiadas. Fin del programa.'
+else
+	echo 'Fin del programa.'
 fi
--- libreria.sh
+++ libreria.sh
@@ -26,14 +26,15 @@ function f_eres_root {
 
 #Mediante esta función, conseguimos el UUID de un dispositivo de bloque.
 #Acepta como argumento el nombre del dispositivo y devuelve su UUID.
-#Devuelve 1 si no eres root, 2 si el dispositivo no está montado y 0 si
-#no hay ningún error.
+#Devuelve 1 si no eres root, 2 si el dispositivo no está montado, y 0 si no hay ningún error.
 function f_UUID {
 	if [[ $(f_eres_root;echo $?) = 0 ]]
 		then
 			if [[ $(f_esta_montado $1;echo $?) = 0 ]]
 				then
-					blkid $1 | awk '{print $2}' | egrep -o '[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}'
+					var='[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}'
+					blkid $1 | awk '{print $2}' | egrep -o $var
+					return 0
 				else
 					echo 'El dispositivo no está montado'
 					return 2
@@ -48,7 +49,7 @@ function f_UUID {
 #Acepta como argumento el nombre del paquete.
 #Devuelve 0 si está instalado y 1 si no lo está.
 function f_esta_instalado {
-	if [[ $(dpkg-query -l | egrep $1 | awk '{print $2}') = $1 ]]
+	if [[ $(dpkg-query -l | awk '{print $2}' | egrep ^$1$) = $1 ]]
 		then
 			return 0
 		else
@@ -65,7 +66,7 @@ function f_instalar {
 		then
 			if [[ $(f_eres_root;echo $?) = 0 ]]
 				then
-					apt-get install -y $1
+					apt-get install -y $1 > /dev/null
 					return 0
 				else
 					echo 'No eres root'
@@ -81,7 +82,7 @@ function f_instalar {
 #Acepta como argumento el nombre del dispositivo de bloque.
 #Devuelve 0 si está montado y 1 si lo está.
 function f_esta_montado {
-	if [[ $(df -h $1) ]]
+	if [[ $(df | egrep $1) ]]
 		then
 			return 0
 		else
@@ -160,3 +161,204 @@ function f_actualizar_paquete {
 	fi
 }
 
+#Esta función crea un sistema de ficheros en un dispositivo de bloque.
+#Acepta como primer argumento el nombre del volumen, y como segundo argumento, el tipo de sistema de ficheros
+#(ext2, ext3, ext4 o vfat).
+#Devuelve 0 al crear el sistema de ficheros, 1 si no eres root, 2 si no existe ese volumen,
+#3 si no está instalado el paquete e2fsprogs o dosfstools (necesarios para crear el sistema de ficheros),
+#y 4 si no se ha introducido un tipo de sistema de ficheros de entre los especificados en este comentario.
+function f_crea_fs {
+	if [[ $(f_eres_root;echo $?) = 0 ]]; then
+		if [[ $(blkid $1) ]]; then
+			if [[ $2 = 'ext2' || $2 = 'ext3' || $2 = 'ext4' ]]; then
+				if [[ $(f_esta_instalado e2fsprogs; echo $?) = 0 ]]; then
+					mkfs.$2 $1 > /dev/null
+					return 0
+				else
+					echo 'El paquete e2fsprogs no está instalado.'
+					return 3
+					exit
+				fi
+			elif [[ $2 = 'vfat' ]]; then
+				if [[ $(f_esta_instalado dosfstools;echo $?) = 0 ]]; then
+					mkfs.$2 $1
+					return 0
+				else
+					echo 'El paquete dosfstools no está instalado.'
+					return 3
+					exit
+				fi
+			else
+				echo 'Tipo de sistema de ficheros incorrecto.'
+				return 4
+				exit
+			fi
+		else
+			echo 'Nombre de dispositivo incorrecto.'
+			return 2
+			exit
+		fi
+	else
+		echo 'No eres root'
+		return 1
+	fi
+}
+
+#Esta función permite añadir una nueva línea al fichero /etc/fstab con los datos del dispositivo de bloque.
+#Recibe como argumentos los valores correspondientes a los campos del fichero fstab y en ese mismo orden
+#(UUID, punto de montaje, tipo de sistema de ficheros, opciones de montaje, dump y chequeo).
+#Devuelve 0 al añadir la línea al fichero y 1 si ya existe esa línea.
+function f_nueva_linea_fstab {
+	if [[ $(egrep $1 /etc/fstab;echo $?) = 1 ]]
+		then
+			echo $1	$2	$3	$4	$5	$6 >> /etc/fstab
+			return 0
+		else
+			return 1
+	fi
+}
+
+#Esta función permite añadir opciones de cuotas para usuarios y grupos en el fichero /etc/fstab.
+#Acepta como argumento la UUID del volumen al que se quiere aplicar cuotas.
+#Devuelve 0 al añadir las opciones y 1 si la UUID es incorrecta.
+function f_cuotas_fstab {
+	if [[ $(egrep $1 /etc/fstab) ]]
+		then
+			opciones=$(egrep $1 /etc/fstab | awk '{print $4}')
+			sed -i 's/'$opciones'/&,usrquota,grpquota/' /etc/fstab
+			return 0
+		else
+			echo 'La UUID es incorrecta.'
+			return 1
+	fi
+}
+
+#Esta función crea los ficheros aquota.user y aquota.group.
+#Acepta como argumento el punto de montaje del volumen.
+#Devuelve 0 al generar los ficheros y 1 si el punto de montaje es incorrecto.
+function f_habilita_quota {
+	if [[ $(lsblk -f | egrep $1) ]]; then
+		quotacheck -ug $1 > /dev/null
+		quotaon $1
+		return 0
+	else
+		echo 'El punto de montaje es incorrecto.'
+		return 1
+	fi
+}
+
+#Esta función comprueba si existe un determinado fichero.
+#Acepta como primer argumento el fichero que se quiere buscar, y como segundo argumento, la ruta absoluta
+#del directorio donde se quiere buscar el fichero, que por defecto será donde se encuentre el usuario.
+#Devuelve 0 si el fichero existe, 1 si no existe y 2 si no existe el directorio introducido.
+function f_existe_fichero {
+	if [[ $2 = $null ]]; then
+		if [[ -e ./$1 ]]; then
+			return 0
+		else
+			return 1
+		fi
+	else
+		if [[ $(f_existe_directorio $2;echo $?) = 0 ]]; then
+			if [[ -e $2/$1 ]]; then
+				return 0
+			else
+				return 1
+			fi
+		else
+			return 2
+		fi
+	fi
+}
+
+#Esta función crea una plantilla de cuotas a gusto del usuario mediante un simple cuestionario,
+#para después emplearla en otros usuarios o grupos.
+#Acepta como primer argumentola ruta del directorio en la que se quiere crear la plantilla.
+function f_plantilla_quota {
+	echo '¿Desea aplicar cuotas a un usuario o a un grupo? (usuario/grupo)'
+	read resp1
+	while [[ $resp1 != 'usuario' && $resp1 != 'grupo' ]];
+	do
+		echo 'Respuesta incorrecta. Introduzca una de las opciones.';
+		read resp1;
+	done
+	if [[ $resp1 = 'usuario' ]]; then
+		var1='-u'
+                echo 'Nombre o UID del usuario (por defecto, se creará el usuario "plantilla"):'
+                read nombre
+		if [[ $nombre = $null ]]; then
+			useradd plantilla 2> /dev/null
+			nombre=plantilla
+		fi
+        else
+                var1='-g'
+                echo 'Nombre o GUID del grupo (por defecto, se creará el grupo "plantilla"):'
+                read nombre
+		if [[ $nombre = $null ]]; then
+			groupadd plantilla 2> /dev/null
+			nombre=plantilla
+		fi
+	fi
+	echo '¿Desea aplicar una cuota de bloques o de inodos? (bloques/inodos)'
+	read resp2
+	while [[ $resp2 != 'bloques' && $resp2 != 'inodos' ]];
+	do
+		echo 'Respuesta incorrecta. Introduzca una de las opciones.'
+		read resp2
+	done
+	if [[ $resp2 = 'bloques' ]]; then
+		var2='-b'
+	else
+		var2='-i'
+	fi
+	echo '¿Desea aplicar cuotas blandas? (s/n)'
+	read resp3
+	if [[ $resp3 = 's' ]]; then
+		var3='-q'
+		echo '¿Qué tamaño desea asignarle a la cuota?'
+		read qsize
+		echo '¿Desea aplicar cuotas duras? (s/n)'
+		read resp4
+		if [[ $resp4 = 's' ]]; then
+			var4='-l'
+			echo '¿Qué tamaño desea asignarle a la cuota?'
+			read lsize
+			quotatool $var1 $nombre $var2 $var3 $qsize $var4 $lsize $1
+			quotaon $1 &> /dev/null
+			return 0
+		else
+			quotatool $var1 $nombre $var2 $var3 $qsize $1
+			quotaon $1 &> /dev/null
+			return 0
+		fi
+	else
+		echo '¿Desea aplicar cuotas duras? (s/n)'
+		read resp4
+		if [[ $resp4 = 's' ]]; then
+			var3='-l'
+			echo '¿Qué tamaño desea asignarle a la cuota?'
+			read lsize
+			quotatool $var1 $nombre $var2 $var3 $lsize $1
+			quotaon $1 &> /dev/null
+			return 0
+		fi
+	fi
+}
+
+#Esta función copia las cuotas de un usuario a un número de usuarios dado.
+#Acepta como argumento el número de usuarios a los que se quiere copiar las cuotas.
+function f_copia_cuotas {
+	echo 'Introduce el nombre del usuario cuyas cuotas quiere copiar:'
+	read nombre
+	for num in $(seq 1 $1)
+	do
+		echo "Nombre de usuario al que quiere aplicar las cuotas:"
+		read usuario
+		if [[ $(awk -F: '{print $1}' /etc/passwd | egrep $usuario) ]]; then
+			edquota -p $nombre $usuario
+		else
+			echo 'Ese usuario no existe.'
+			continue
+		fi;
+	done
+}
